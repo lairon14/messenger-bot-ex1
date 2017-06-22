cffc614aec102b018e751e96ae3df8b09e113439
Lairon Acosta <lacosta@novopayment.com>
2017-06-22 16:25:43 -0400

    changes on app.js

--- app.js
+++ app.js
@@ -520,16 +520,132 @@ function sendFileMessage(recipientId) {
  *
  */
 function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText,
-      metadata: "DEVELOPER_DEFINED_METADATA"
-    }
-  };
+  if(messageText==="hola"){
 
+      var messageData = {
+          recipient:{
+              id:"RECIPIENT_ID"
+          }, message: {
+              attachment: {
+                  type: "template",
+                      payload: {
+                      template_type: "list",
+                          top_element_style: "compact",
+                          elements: [
+                          {
+                              title: "Classic White T-Shirt",
+                              image_url: "https://peterssendreceiveapp.ngrok.io/img/white-t-shirt.png",
+                              subtitle: "100% Cotton, 200% Comfortable",
+                              default_action: {
+                                  type: "web_url",
+                                  url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                                  messenger_extensions: true,
+                                  webview_height_ratio: "tall",
+                                  fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                              },
+                              buttons: [
+                                  {
+                                      title: "Buy",
+                                      type: "web_url",
+                                      url: "https://peterssendreceiveapp.ngrok.io/shop?item=100",
+                                      messenger_extensions: true,
+                                      webview_height_ratio: "tall",
+                                      fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                                  }
+                              ]
+                          },
+                          {
+                              title: "Classic Blue T-Shirt",
+                              image_url: "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                              subtitle: "100% Cotton, 200% Comfortable",
+                              default_action: {
+                                  type: "web_url",
+                                  url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                                  messenger_extensions: true,
+                                  webview_height_ratio: "tall",
+                                  fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                              },
+                              buttons: [
+                                  {
+                                      title: "Buy",
+                                      type: "web_url",
+                                      url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                                      messenger_extensions: true,
+                                      webview_height_ratio: "tall",
+                                      fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                                  }
+                              ]
+                          },
+                          {
+                              title: "Classic Black T-Shirt",
+                              image_url: "https://peterssendreceiveapp.ngrok.io/img/black-t-shirt.png",
+                              subtitle: "100% Cotton, 200% Comfortable",
+                              default_action: {
+                                  type: "web_url",
+                                  url: "https://peterssendreceiveapp.ngrok.io/view?item=102",
+                                  messenger_extensions: true,
+                                  webview_height_ratio: "tall",
+                                  fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                              },
+                              buttons: [
+                                  {
+                                      title: "Buy",
+                                      type: "web_url",
+                                      url: "https://peterssendreceiveapp.ngrok.io/shop?item=102",
+                                      messenger_extensions: true,
+                                      webview_height_ratio: "tall",
+                                      fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                                  }
+                              ]
+                          },
+                          {
+                              title: "Classic Gray T-Shirt",
+                              image_url: "https://peterssendreceiveapp.ngrok.io/img/gray-t-shirt.png",
+                              subtitle: "100% Cotton, 200% Comfortable",
+                              default_action: {
+                                  type: "web_url",
+                                  url: "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                                  messenger_extensions: true,
+                                  webview_height_ratio: "tall",
+                                  fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                              },
+                              buttons: [
+                                  {
+                                      title: "Buy",
+                                      type: "web_url",
+                                      url: "https://peterssendreceiveapp.ngrok.io/shop?item=103",
+                                      messenger_extensions: true,
+                                      webview_height_ratio: "tall",
+                                      fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                                  }
+                              ]
+                          }
+                      ],
+                          buttons: [
+                          {
+                              title: "View More",
+                              type: "postback",
+                              payload: "payload"
+                          }
+                      ]
+                  }
+              }
+          }
+
+      };
+
+  } else {
+
+      var messageData = {
+          recipient: {
+              id: recipientId
+          },
+          message: {
+              text: messageText,
+              metadata: "DEVELOPER_DEFINED_METADATA"
+          }
+      };
+  }
   callSendAPI(messageData);
 }
 
